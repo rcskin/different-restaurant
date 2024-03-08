26dd23341f2cdef376161311edb1aea4534edffe
Rachel Skinner <skinner.rachelc@gmail.com>
2024-03-08 14:14:34 +0000

    added component and CSS for footer

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom"; // Import Routes
+import Home from "./components/Home";
+import Menu from "./components/Menu";
+import Events from "./components/Events";
+import Contact from "./components/Contact";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav className="navbar">
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/menu">Menu</Link>
+            </li>
+            <li>
+              <Link to="/events">Events</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+          </ul>
+        </nav>
+        <div className="logo-container">
+          
+        </div>
+        <Routes>
+          {" "}
+          {/* Wrap Routes around Route components */}
+          <Route path="/" element={<Home />} /> {/* Use "element" prop */}
+          <Route path="/menu" element={<Menu />} />
+          <Route path="/events" element={<Events />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+        <footer className="footer">
+          <div class="copyright">
+            &copy; 2024 Different Restaurant. All rights reserved.
+          </div>
+          <p>Website created by Rachel Skinner 
+          <a href="https://next-portfolio-rcskin.vercel.app/" target="blank"> - Web developer </a></p>
+        </footer>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/Contact.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Contact = () => {
+  return (
+    <div>
+      <h2>Welcome to Contact</h2>
+      <p>This is the home page of our website.</p>
+    </div>
+  );
+};
+
+export default Contact;
